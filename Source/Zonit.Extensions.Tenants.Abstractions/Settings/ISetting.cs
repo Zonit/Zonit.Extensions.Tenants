62b04d19d171dd689f5488ad92973a19b48efb8a
Dawid Haładus <alerinpoland@gmail.com>
2025-06-09 19:50:58 +0200

    Aktualizacja wersji Assembly i zmiany w interfejsach
    Zmieniono wersję Assembly w pliku `Directory.Packages.props` na `1.0.2`.
    Dodano właściwość `Value` typu `object?` w interfejsie `ISetting`, a w interfejsie `ISetting<T>` zmieniono definicję `Value` na nową, co ukrywa implementację z interfejsu bazowego.
    Wprowadzono implementację `Value` w interfejsie `ISetting`, która zwraca wartość z `ISetting<T>`.
    W klasie `Setting<T>` dodano modyfikator `virtual` do `Value`, co umożliwia nadpisanie w klasach pochodnych oraz zainicjowano ją wartością domyślną `default!`.

--- Source/Zonit.Extensions.Tenants.Abstractions/Settings/ISetting.cs
+++ Source/Zonit.Extensions.Tenants.Abstractions/Settings/ISetting.cs
@@ -5,12 +5,15 @@ public interface ISetting
     string Key { get; }
     string Name { get; }
     string Description { get; }
+
+    object? Value { get; }
 }
 
 public interface ISetting<T> : ISetting
 {
-    T Value { get; set; }
+    new T Value { get; set; }
     IReadOnlyCollection<T>? Templates { get; }
+    object? ISetting.Value => Value;
 }
 
 public abstract class Setting<T> : ISetting<T>
@@ -18,7 +21,7 @@ public abstract class Setting<T> : ISetting<T>
     public abstract string Key { get; }
     public abstract string Name { get; }
     public abstract string Description { get; }
-    
+
     public virtual T Value { get; set; } = default!;
     public virtual IReadOnlyCollection<T>? Templates { get; } = null;
 }
